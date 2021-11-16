d0b1e7eae9569670fd1a1f14e949c5576647da68
takegawa <takegawa@logical.co.jp>
2021-11-16 14:36:18 +0900

    Initial Commit

---
+++ features/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  features
+//
+//  Created by 竹川　昌邦 on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
